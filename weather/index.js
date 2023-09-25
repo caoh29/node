4ad4461af18865306453dbf1ac3b4c43846255c7
Camilo Ordoñez <cronox20@gmail.com>
2023-09-25 18:42:44 -0400

    done, only missing persistance but dont think is worthy

--- weather/index.js
+++ weather/index.js
@@ -9,19 +9,56 @@ const main = async () => {
     switch (opt) {
       case "1":
         const city = await askForInput("city: ");
-        const data = await finds.getData(city);
-        const { name, latitude, longitude, country, population, is_capital } = data[0];
+        const cityData = await finds.getCityData(city);
+        const weatherData = await finds.getWeatherData(city);
+
+        const info = {
+          ...cityData,
+          ...weatherData
+        };
+
+        const {
+          name,
+          latitude,
+          longitude,
+          country,
+          population,
+          temp,
+          min_temp,
+          max_temp,
+          feels_like,
+          humidity,
+          wind_speed,
+          wind_degrees
+        } = info;
+
         console.log("\n        City Data       ");
         console.log('==========================');
         console.log(`City: ${name}`);
         console.log(`Country: ${country}`);
         console.log(`Population: ${population}`);
-        console.log(`Lat: ${latitude}`);
-        console.log(`Long: ${longitude}`);
-        console.log(`Is Capital: ${is_capital ? "Yes" : "No"}`);
+        console.log(`Lat: ${latitude}°`);
+        console.log(`Long: ${longitude}°`);
+        console.log(`Temperature: ${temp} °C`);
+        console.log(`Min Temperature: ${min_temp} °C`);
+        console.log(`Max Temperature: ${max_temp} °C`);
+        console.log(`Feels Like: ${feels_like} °C`);
+        console.log(`Humidity: ${humidity} φ`);
+        console.log(`Wind Speed: ${wind_speed} km/h`);
+        console.log(`Wind Degrees: ${wind_degrees}°`);
         break;
+      
       case "2":
-        console.log("2");
+        const cities = await finds.getHistory();
+        console.log("\n        History       ");
+        console.log('==========================');
+        if (cities.length === 0) {
+          console.log("No cities searched yet");
+          break;
+        }
+        cities.forEach((city, index) => {
+          console.log(`${index + 1}. ${city}`);
+        });
         break;
     }
     if (opt !== "0") await pause();
--- weather/models/find.js
+++ weather/models/find.js
@@ -8,15 +8,38 @@ class Finds {
     this.history = [];
   }
 
-  getData = async (city) => {
+  getHistory = async () => {
+    return this.history;
+  }
+
+  getCityData = async (city) => {
     if (city.length === 0) return null;
+    this.history.push(city);
+    this.city = city;
     try {
-      const res = await axios.get(`https://api.api-ninjas.com/v1/city?name=${city}`,{
+      const res = await axios.get(`https://api.api-ninjas.com/v1/city?name=${city}`, {
         headers: {
           'X-Api-Key': `${process.env.API_KEY}`,
         }
       });
-      return res.data;
+      
+      const cityData = res.data[0];
+      return cityData;
+    } catch (error) {
+      return `${error}`;
+    }
+  }
+
+  getWeatherData = async (city) => {
+    if (city.length === 0) return null;
+    try {
+      const res= await axios.get(`https://api.api-ninjas.com/v1/weather?city=${city}`, {
+        headers: {
+          'X-Api-Key': `${process.env.API_KEY}`,
+        }
+      });
+      const weatherData = res.data;
+      return weatherData;
     } catch (error) {
       return `${error}`;
     }
