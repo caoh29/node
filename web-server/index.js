fb4b251e3719a361cf1e2b9e6fe1dc514150f391
Camilo Ordoñez <cronox20@gmail.com>
2023-10-02 12:50:10 -0400

    playing with web server, deployment using render

--- web-server/index.js
+++ web-server/index.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const port = process.env.PORT;
+
+
+app.use(express.static('template'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/template/index.html');
 });
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/template/elements.html');
+});
+
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/template/generic.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
