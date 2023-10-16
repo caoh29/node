2841dbd7acc3ea51d66034e9585725861979dbf6
Camilo Ordoñez <cronox20@gmail.com>
2023-10-15 21:58:15 -0400

    fix

--- rest-server/middlewares/validate-JWT.js
+++ rest-server/middlewares/validate-JWT.js
@@ -8,6 +8,7 @@ const validateJWT = async (req, res, next) => {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     const { _id } = payload;
     const user = await User.search(_id);
+    if (!user) return res.status(401).json({ message: 'Invalid user' });
     req.user = user;
     req.user.auth = true;
     next();
